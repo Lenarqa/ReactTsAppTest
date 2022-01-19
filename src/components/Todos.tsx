6050f29695c04e2b30f79711d8b770469b7576a6
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-01-19 17:02:55 +0700

    create siple todo component

---
+++ src/components/Todos.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Todos: React.FC<{ items: string[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((todo) => (
+        <li key="todo">{todo}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
